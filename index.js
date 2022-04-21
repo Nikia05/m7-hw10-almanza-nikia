6eb2a7b72050e2396eb588535e2544a36ddfc8c1
Nikia05 <87290039+Nikia05@users.noreply.github.com>
2022-04-21 10:45:59 -0700

    added files

---
+++ index.js
@@ -0,0 +1,43 @@
+// acquire references to page elements
+var nameSpan = document.querySelector('span')
+var formEl = document.querySelector('form')
+var clear = document.querySelector('#clear')
+var textarea = document.querySelector('textarea')
+
+// Retrieve name and note content from cookies and localstorage
+// Then apply them to elements on the page
+// YOUR CODE HERE
+window.onload = function() {
+  document.getElementById('notes-area').value = localStorage.getItem("NotesData");
+  nameSpan.innerHTML = decodeURIComponent(document.cookie);
+
+};
+formEl.onsubmit = function(e) {
+  // prevents form submission
+  e.preventDefault()
+  // save name element's content to cookies
+  // save textarea's content to localstorage
+  // YOUR CODE HERE  
+  document.cookie = nameSpan.innerHTML
+  localStorage.setItem("NotesData", document.getElementById('notes-area').value);
+  // triggers thumbs up animation
+  this.elements.save.classList.add('emoji')
+}
+
+clear.onclick = function() {
+  // Clear textarea's value
+  // Clear localstorage's content
+  // YOUR CODE HERE
+  document.getElementById('notes-area').value = "";
+  localStorage.removeItem("NotesData");
+  // triggers thumbs up animation
+  this.classList.add('emoji')
+}
+
+// this code allows repeated thumbs up animations
+function endThumbsUp() {
+  this.classList.remove('emoji')
+}
+
+formEl.elements.save.onanimationend = endThumbsUp
+clear.onanimationend = endThumbsUp
